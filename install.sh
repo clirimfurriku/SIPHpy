ae2e25f1f65f7186b8d9cf28d9a3e3e570c4f6ed
Çlirim Furriku <clirimfurriku@users.noreply.github.com>
2018-11-16 16:27:06 +0100

    Changed to new app
    Improved to install new app called SIPHpy

--- install.sh
+++ install.sh
@@ -6,22 +6,23 @@ pkg install -y wget && pkg install -y python && pkg install -y curl
 echo _____________________________________
 echo installing commands
 echo ______________________________________
-wget https://raw.githubusercontent.com/clirimfurriku/payloader/master/clirimfurriku
+wget https://raw.githubusercontent.com/clirimfurriku/SIPHpy/master/siphpy
 
-cp /data/data/com.termux/files/home/clirimfurriku /data/data/com.termux/files/usr/bin/clirimfurriku
+cp /data/data/com.termux/files/home/siphpy /data/data/com.termux/files/usr/bin/siphpy
 
 wget https://raw.githubusercontent.com/clirimfurriku/payloader/master/ip.py
 
 cp /data/data/com.termux/files/home/ip.py /data/data/com.termux/files/usr/bin/ip.py
 
-chmod +x /data/data/com.termux/files/usr/bin/clirimfurriku
+chmod +x /data/data/com.termux/files/usr/bin/siphpy
 
-chmod +x x /data/data/com.termux/files/usr/bin/ip.py
+chmod +x /data/data/com.termux/files/usr/bin/ip.py
 
 rm -f /data/data/com.termux/files/home/ip.py
-rm -f /data/data/com.termux/files/home/clirimfurriku
+rm -f /data/data/com.termux/files/home/siphpy
 
 echo. ____________________________________________
 echo finished installing
 echo ____________________________________________
-echo to start enter command "clirimfurriku"
+echo to start enter command "siphpy"
+echo.
