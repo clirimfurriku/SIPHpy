03f22174dd1c1a06e202d4cb23e39053bed06b7d
Çlirim Furriku <clirimfurriku@users.noreply.github.com>
2018-11-29 13:34:56 +0100

    Finale Release (moved to 2.py)
    Impossible to use python pool on Termux so this is the end of Termux project

--- ip.py
+++ ip.py
@@ -4,15 +4,12 @@ import httplib2
 import subprocess
 import multiprocessing as mp
 
-
-# Lest begin
-
 def first():
     print(" ")
     print(" ")
-    print("           Welcome to IP Header scanner        ")
+    print("            Welcome to IP Header scanner         ")
     print("_______________________________________________")
-    print("            Scanner IP Headers Python          ")
+    print(" Scanner IP Headers Python")
     print("_______________________________________________")
     print(" ")
     print("    .--. .-..---. .-..-.             ")
@@ -27,33 +24,32 @@ def first():
     print(" ")
     print("               Never change my name            ")
     print("_______________________________________________")
-    print("        My website is www.bardtech.com         ")
+    print("       My website is www.bardtech.com.       ")
     print("_______________________________________________")
-    print("                Project source link.           ")
-    print("    https://github.com/clirimfurriku/SIPHpy    ")
-    print("_______________________________________________")
-    print(" ")
+    print("                Project source link.     ")
+    print("    https://github.com/clirimfurriku/SIPHpy ")
     print("_______________________________________________")
     print(" ")
-    print("After finishing see pld.txt in Downloads folder")
+    print("_______________________________________________")
+    print(" ")
+    print(" After finishing see pld.txt.txt in Downloads folder")
 
 
-def hget(ip, rsp, proxy, set_proxy, port):
-    url = "http://%s" % ip
+def hget(ip, rsp, proxy, sprx, port):
+    url = "http://%s" % (ip)
     if proxy == 0:
         proxing = httplib2.Http(timeout=1)
     elif proxy == 1:
-        proxing = httplib2.Http(proxy_info=httplib2.ProxyInfo(httplib2.socks.PROXY_TYPE_HTTP_NO_TUNNEL, set_proxy, port),
-                                timeout=1)
+        proxing = httplib2.Http(proxy_info=httplib2.ProxyInfo(httplib2.socks.PROXY_TYPE_HTTP_NO_TUNNEL, sprx, port),timeout=1)
     http_interface = proxing
     try:
         response, content = http_interface.request(url, method="HEAD")
-        print("\r[i] Response status: %d - %s for %s" % (response.status, response.reason, ip), end='')
+        print("\r[i] Response status: %d - %s for %s" % (response.status, response.reason, ip), end="")
         rsp.append("[+] " + ip + " Status: " + str(response.status) + " - " + response.reason)
     except httplib2.ServerNotFoundError as e:
         print(e.message)
     except httplib2.socket.error:
-        print("\r[i] Response status: Error - Unreachable for %s" % ip, end='')
+        print("\r[i] Response status: Error - Unreachable for %s" % ip, end="")
     except httplib2.httplib.ResponseNotReady:
         pass
     return rsp
@@ -66,7 +62,7 @@ def ip_calc(a, times):
     ip = []
     thend = 0
     stops = 0
-    print('\r[i] Calculating  %d.%d.%d.%d to %d.%d.%d.%d' % (a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]), end="")
+    print('\r[i] Checking from  %d.%d.%d.%d to %d.%d.%d.%d' % (a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]), end="")
     ip.append('%d.%d.%d.%d' % (a[0], a[1], a[2], a[3]))
     if a[0] > 255 or a[1] > 255 or a[2] > 255 or a[3] > 255:
         thend = 1
@@ -88,17 +84,20 @@ def ip_calc(a, times):
             break
         times += 1
         ip.append('%d.%d.%d.%d' % (a[0], a[1], a[2], a[3]))
-        if stops == 100:
+        if stops == 50:
             break
         stops += 1
     return a, times, ip, thend
 
 
-def scaner(ip, proxy, sprx, port):
+def scaner(ip, proxy, sprx, port, output):
     rsp = []
     for adress in ip:
         rsp = hget(adress, rsp, proxy, sprx, port)
-    return rsp
+    for grsp in rsp:
+        print(grsp)
+        #subprocess.call('echo %s >> pld.txt' % (grsp), shell=True)
+        output.put((rsp))
 
 
 def get_ip():
@@ -116,17 +115,6 @@ def get_ip():
         a.append(int(element))
     del b
     print(" ")
-    print("How many threads do you want to use?")
-    print(" ")
-    print("Recommended 50")
-    print(" ")
-    try:
-        threads = int(input("Enter MAX number of threads to use: "))
-    except ValueError:
-        threads = 50
-    print(" ")
-    print("_______________________________________________")
-    print(" ")
     print("Do you want to use proxy?")
     print(" ")
     print("Options")
@@ -134,69 +122,95 @@ def get_ip():
     print("0) Do not use proxy")
     print("1) Use proxy")
     print(" ")
-    try:
-        proxy = int(input("Enter Option (0 or 1): "))
-    except ValueError:
-        proxy = 0
+    proxy = int(input("Enter Option (0 or 1): "))
     print(" ")
     print("_______________________________________________")
     print("_______________________________________________")
     print(" ")
     if proxy == 1:
-        set_proxy = input("Enter Proxy IP:Port (*.*.*.*:****): ")
-        helper = re.split("\:", set_proxy)
-        set_proxy = helper[0]
+        sprx = input("Enter Proxy IP:Port (*.*.*.*:****): ")
+        print("_______________________________________________")
+        print("_______________________________________________")
+        print(" ")
+        helper = re.split("\:", sprx)
+        sprx = helper[0]
         port = int(helper[1])
     elif proxy == 0:
-        set_proxy = 0
+        sprx = 0
         port = 0
-    return a, proxy, set_proxy, port, threads
+    return a, proxy, sprx, port
 
 
-def mthrd(a, proxy, sprx, port, threads):
-    print("Starting " + str(threads) + " Threads")
-    pool = mp.Pool(processes=threads)
-    print("Threads started")
+def mthrd(a, proxy, sprx, port):
     thend: int = 0
     times: int = 1
+    b = a
     t = []
+    output = mp.Queue()
+    whi = 0
     while thend == 0:
         (a, times, ip, thend) = ip_calc(a, times)
-        t.append(ip)
-    time.sleep(5)
-    results = [pool.apply_async(scaner, args=(ipd, proxy, sprx, port,)) for ipd in t]
-    output = [p.get() for p in results]
-    subprocess.call('echo -------------------- >> pld.txt', shell=True)
-    subprocess.call('echo --------SIPHpy------ >> pld.txt', shell=True)
-    subprocess.call('echo -------------------- >> pld.txt', shell=True)
-    subprocess.call('echo -----Created By:---- >> pld.txt', shell=True)
-    subprocess.call('echo ---Clirim Furriku--- >> pld.txt', shell=True)
-    subprocess.call('echo -------------------- >> pld.txt', shell=True)
-    subprocess.call('echo -------Results:----- >> pld.txt', shell=True)
-    for success in output:
-        for individual_ip in success:
-            subprocess.call('echo %s >> pld.txt' % individual_ip, shell=True)
-            print(individual_ip)
+        t.append(mp.Process(target=scaner, args=(ip, proxy, sprx, port, output)))
+    for thred in t:
+        thred.start()
+        if whi == 100:
+            time.sleep(30)
+            whi = 0
+        whi += 1
+    for thred in t:
+        if thred.is_alive():
+            thred.join(timeout=2)
+    finished = 0
+    while finished==0:
+        if 'started'in mp.active_children():
+            print('\rWorking |', end="")
+            time.sleep(0.1)
+            print('\rWorking /', end="")
+            time.sleep(0.1)
+            print('\rWorking -', end="")
+            time.sleep(0.1)
+            print('\rWorking \\', end="")
+            time.sleep(0.1)
+        else:
+            finished = 1
+            print('\rDone!     ', end="")
+            time.sleep(1)
+    print("Finished")
+    results = [output.get() for p in t]
+    subprocess.call('echo ____________________________ >> pld.txt', shell=True)
+    subprocess.call('echo ___________SIPHpy___________ >> pld.txt', shell=True)
+    subprocess.call('echo ____________________________ >> pld.txt', shell=True)
+    subprocess.call('echo _________Created By:________ >> pld.txt', shell=True)
+    subprocess.call('echo _______Clirim Furriku_______ >> pld.txt', shell=True)
+    subprocess.call('echo ____________________________ >> pld.txt', shell=True)
+    subprocess.call('echo Checking from  %d.%d.%d.%d to %d.%d.%d.%d >> pld.txt' % (b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]), shell=True)
+    subprocess.call('echo ____________________________ >> pld.txt', shell=True)
+    subprocess.call('echo __________Results:__________ >> pld.txt', shell=True)
+    subprocess.call('echo ____________________________ >> pld.txt', shell=True)
+    for result in results:
+        for ip in result:
+            print(ip)
+            subprocess.call('echo %s >> pld.txt' % (ip), shell=True)
     return times
 
 
 def main():
-    (a, proxy, set_proxy, port, threads) = get_ip()
+    (a, proxy, sprx, port) = get_ip()
     start = time.time()
-    times = mthrd(a, proxy, set_proxy, port, threads)
-    subprocess.call('echo [i] Checked %d IPs >> pld.txt' % times, shell=True)
-    print('\n\n[i] Checked ' + str(times) + ' IPs')
+    times = mthrd(a, proxy, sprx, port)
+    subprocess.call('echo ____________________________ >> pld.txt', shell=True)
+    subprocess.call('echo [i] Checked %d IPs >> pld.txt' % (times), shell=True)
+    print('[i] Checked ' + str(times) + ' IPs')
     print("")
     end = time.time()
     took = end - start
     print('[i] Total time to Finish was: ' + str(took))
-    subprocess.call('echo [i] Time Taken To Check IPs was %s seconds >> pld.txt' % took, shell=True)
+    subprocess.call('echo [i] Time Taken To Check IPs was %s >> pld.txt' % (took), shell=True)
 
 
 if __name__ == '__main__':
     first()
     # mp.set_start_method('spawn')
-    # mp.set_start_method('forkserver')
-    # mp.set_start_method('fork')
+    # mp.get_all_start_methods()
     # mp.freeze_support()
-    main()
+    main(
